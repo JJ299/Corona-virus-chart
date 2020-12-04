6fbdef68e5f0c96cf85812e1cafb895ecf03ffc5
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-12-03 22:31:39 -0500

    Started working on main.js

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const country_name_element = document.querySelector(".country .name")
+const total_cases_element = document.querySelector(".total-cases .value")
+const new_cases_element = document.querySelector(".total-cases .new-value")
